924b99b766cd8e7cbe6e13f9f317e60255be650d
Mohamed KADI <handskadi@gmail.com>
2025-05-25 16:13:39 +0100

    all tasks

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,19 @@
+from seed import connect_to_prodev
+
+def stream_users_in_batches(batch_size):
+    conn = connect_to_prodev()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data")
+    while True:
+        rows = cursor.fetchmany(batch_size)
+        if not rows:
+            break
+        yield rows
+    cursor.close()
+    conn.close()
+
+def batch_processing(batch_size):
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
+            if int(user['age']) > 25:
+                print(user)
